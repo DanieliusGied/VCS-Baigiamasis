d1e0843bebdd1d1af4e78dfa9f77cbdf68946e24
LAPTOP-M8A7SOK1\danie <danielius.giedraitis@gmail.com>
2022-04-11 16:28:27 +0300

    Set up CharacterInfoScreenActivity.java

--- app/src/main/java/lt/vcs/baigiamasis/CharacterInfoScreenActivity.java
+++ app/src/main/java/lt/vcs/baigiamasis/CharacterInfoScreenActivity.java
@@ -1,14 +1,55 @@
 package lt.vcs.baigiamasis;
 
+import static lt.vcs.baigiamasis.MainActivity.character;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class CharacterInfoScreenActivity extends AppCompatActivity {
+
+    MaterialButton materialButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_info_screen);
+
+        setUpText();
+        setUpCloseButton();
     }
+
+    private void setUpText() {
+        TextView textViewName = (TextView) findViewById(R.id.textViewCharacterInfoScreenName);
+        textViewName.setText("Name: " + character.getName());
+        TextView textViewHP = (TextView) findViewById(R.id.textViewCharacterInfoScreenHP);
+        textViewHP.setText("HP: " + character.getCurrentHealth() + " / " + character.getMaxHealth());
+        TextView textViewMP = (TextView) findViewById(R.id.textViewCharacterInfoScreenMP);
+        textViewMP.setText("MP: null");
+        TextView textViewXP = (TextView) findViewById(R.id.textViewCharacterInfoScreenXP);
+        textViewXP.setText("XP: null");
+        TextView textViewSTR = (TextView) findViewById(R.id.textViewCharacterInfoScreenSTR);
+        textViewSTR.setText("STR: " + character.getStatStr());
+        TextView textViewCON = (TextView) findViewById(R.id.textViewCharacterInfoScreenCON);
+        textViewCON.setText("CON: " + character.getStatCon());
+        TextView textViewDEX = (TextView) findViewById(R.id.textViewCharacterInfoScreenDEX);
+        textViewDEX.setText("DEX: " + character.getStatDex());
+    }
+
+    private void setUpCloseButton(){
+        materialButton = findViewById(R.id.materialButtonCharacterInfoScreenClose);
+        materialButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    };
 }
--- app/src/main/java/lt/vcs/baigiamasis/zaidimukasclasses/Character.java
+++ app/src/main/java/lt/vcs/baigiamasis/zaidimukasclasses/Character.java
@@ -1,5 +1,6 @@
 package lt.vcs.baigiamasis.zaidimukasclasses;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static lt.vcs.baigiamasis.zaidimukasclasses.ItemType.ARMOR;
@@ -7,14 +8,17 @@ import static lt.vcs.baigiamasis.zaidimukasclasses.ItemType.WEAPON;
 import static lt.vcs.baigiamasis.MainActivity.*;
 
 public class Character {
-    public String name;
-    public int[] stats = new int[3];
-    public int level;
-    public int health;
-    public HashMap<ItemType, Item> inventory;
-    static public int damage;
-    static public int armor;
-    static int gold;
+    private String name;
+    private int statStr;
+    private int statCon;
+    private int statDex;
+    private int level;
+    private int maxHealth;
+    private int currentHealth;
+    private HashMap<ItemType, Item> inventory;
+    private int damage;
+    private int armor;
+    private int gold;
 
     public Character(String name){
         System.out.println("Hello and welcome :) Please enter your character's name:");
@@ -23,36 +27,35 @@ public class Character {
         inventory.put(WEAPON, dagger);
         inventory.put(ARMOR, clothShirt);
         level = 1;
-        setStats(10, 10, 10);
-        setHealth();
+
+        this.statStr = 10;
+        this.statCon = 10;
+        this.statDex = 10;
+
+        setMaxHealth();
+        setCurrentHealth();
         setArmor();
         setGold();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setStats(int str, int con, int dex){
-        this.stats[0] = str;
-        this.stats[1] = con;
-        this.stats[2] = dex;
     }
 
     public void setGold(){
         this.gold = 10;
     }
 
-    public void setHealth(){
-        this.health = this.level * 5 + (this.stats[1] - 10) * 5;
+    public void setMaxHealth(){
+        this.maxHealth = this.level * 5 + (this.statCon - 10) * 5;
+    }
+
+    public void setCurrentHealth(){
+        this.currentHealth = this.maxHealth;
     }
 
     public void setArmor(){
-        this.armor = inventory.get(ARMOR).armor + (this.stats[2] - 10);
+        this.armor = inventory.get(ARMOR).armor + (this.statDex - 10);
     }
 
     public int calculateDamage(){
-        this.damage = inventory.get(WEAPON).calculateWeaponDamage() + (stats[0] - 10);
+        this.damage = inventory.get(WEAPON).calculateWeaponDamage() + (this.statStr - 10);
         return this.damage;
     }
 
@@ -88,17 +91,17 @@ public class Character {
 //        setHealth();
 //    }
 
-    public void getCharacterData(){
-        System.out.println("");
-        System.out.println("Character name: " + this.name);
-        System.out.println("Character level: " + this.level);
-        System.out.println("Character HP: " + this.health);
-        System.out.println("Character STR: " + this.stats[0]);
-        System.out.println("Character CON: " + this.stats[1]);
-        System.out.println("Character DEX: " + this.stats[2]);
-        System.out.println("Character Armor: " + this.armor);
-        System.out.println("Character Damage: " + "1 - " + (inventory.get(WEAPON).maxDamage + this.stats[0] - 10));
-    }
+//    public void getCharacterData(){
+//        System.out.println("");
+//        System.out.println("Character name: " + this.name);
+//        System.out.println("Character level: " + this.level);
+//        System.out.println("Character HP: " + this.maxHealth);
+//        System.out.println("Character STR: " + this.stats[0]);
+//        System.out.println("Character CON: " + this.stats[1]);
+//        System.out.println("Character DEX: " + this.stats[2]);
+//        System.out.println("Character Armor: " + this.armor);
+//        System.out.println("Character Damage: " + "1 - " + (inventory.get(WEAPON).maxDamage + this.stats[0] - 10));
+//    }
 
 //    public void death(){
 //        System.out.println("Oh no! Your character has died!");
@@ -117,5 +120,77 @@ public class Character {
 
     public void writeCharacterToDatabase(){
         // TODO: 4/11/2022 CREATE THE WRITE TO DATABASE METHOD
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public int getStatStr() {
+        return statStr;
+    }
+
+    public void setStatStr(int statStr) {
+        this.statStr = statStr;
+    }
+
+    public int getStatCon() {
+        return statCon;
+    }
+
+    public void setStatCon(int statCon) {
+        this.statCon = statCon;
+    }
+
+    public int getStatDex() {
+        return statDex;
+    }
+
+    public void setStatDex(int statDex) {
+        this.statDex = statDex;
+    }
+
+    public void setMaxHealth(int maxHealth) {
+        this.maxHealth = maxHealth;
     }
 }
