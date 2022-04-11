b69c443a3774aee90e7b1a21cc8def89f5c11a66
LAPTOP-M8A7SOK1\danie <danielius.giedraitis@gmail.com>
2022-04-11 15:39:41 +0300

    Continue work on SecondScreenActivity.java
    setUpCharacterInfoScreenButton() and setUpInventoryScreenButton() set up and open the Character Info and Inventory screens

--- app/src/main/java/lt/vcs/baigiamasis/SecondScreenActivity.java
+++ app/src/main/java/lt/vcs/baigiamasis/SecondScreenActivity.java
@@ -2,15 +2,21 @@ package lt.vcs.baigiamasis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import static lt.vcs.baigiamasis.MainActivity.character;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import org.w3c.dom.Text;
 
 public class SecondScreenActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton1;
+    FloatingActionButton floatingActionButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +25,31 @@ public class SecondScreenActivity extends AppCompatActivity {
 
         TextView textView = (TextView) findViewById(R.id.textViewSecondScreen);
         textView.setText("Welcome " + character.getName());
+
+        setUpCharacterInfoScreenButton();
+        setUpInventoryScreenButton();
     }
+
+    private void setUpCharacterInfoScreenButton(){
+        floatingActionButton1 = findViewById(R.id.floatingActionButtonCharacterInfoScreen);
+        floatingActionButton1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondScreenActivity.this, CharacterInfoScreenActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setUpInventoryScreenButton(){
+        floatingActionButton2 = findViewById(R.id.floatingActionButtonInventoryScreen);
+        floatingActionButton2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondScreenActivity.this, InventoryScreenActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
