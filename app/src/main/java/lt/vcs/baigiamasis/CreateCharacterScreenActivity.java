6d08343a72f94af20b29bf1a857b6a1801095337
LAPTOP-M8A7SOK1\danie <danielius.giedraitis@gmail.com>
2022-04-11 13:50:15 +0300

    Start working on CreateCharacterScreenActivity.java.java

--- app/src/main/java/lt/vcs/baigiamasis/CreateCharacterScreenActivity.java
+++ app/src/main/java/lt/vcs/baigiamasis/CreateCharacterScreenActivity.java
@@ -2,13 +2,50 @@ package lt.vcs.baigiamasis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class CreateCharacterScreenActivity extends AppCompatActivity {
+
+    MaterialButton materialButton1;
+    MaterialButton materialButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_character_screen);
+        Log.i("TEST_TAG", "Create Char - On Create");
+        setUpCancelButton();
     }
+
+    private void setUpCancelButton(){
+        materialButton2 = findViewById(R.id.materialButtonCreateCharacterScreenCancel);
+        materialButton2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CreateCharacterScreenActivity.this, FirstScreenActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void setUpCreateButton(){
+        materialButton1 = findViewById(R.id.materialButtonCreateCharacterScreenConfirm);
+        materialButton1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CreateCharacterScreenActivity.this, FirstScreenActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    // TODO: 4/11/2022 Import the character class from Zaidimukas project and create a new object here
+    // TODO: 4/11/2022 Save created character into the database
 }
--- app/src/main/java/lt/vcs/baigiamasis/FirstScreenActivity.java
+++ app/src/main/java/lt/vcs/baigiamasis/FirstScreenActivity.java
@@ -2,13 +2,41 @@ package lt.vcs.baigiamasis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class FirstScreenActivity extends AppCompatActivity {
+
+    MaterialButton materialButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen);
+        setUpCreateNewCharacterButton();
+    }
+
+    private void setUpCreateNewCharacterButton(){
+        materialButton = findViewById(R.id.materialButtonFirstScreenCreateNewCharacter);
+        materialButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FirstScreenActivity.this, CreateCharacterScreenActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    // TODO: 4/11/2022 Create a database connection that loads a character from the database
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("TEST_TAG", "FirstScreen On Destroy");
     }
 }
